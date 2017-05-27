eff8bbf69e5ae64d1e51fc26a613385928f7983a
dimensionsjewelry <steve@dimensionsjewelry.com>
2017-05-27 09:22:28 -0400

    Add ConfigurationBuiilder, Services and IGreeter.

---
+++ Greeter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelRelief
+    {
+    public interface IGreeter
+        {
+        string GetGreeting();
+        }
+
+    public class Greeter : IGreeter
+        {
+        public IConfiguration _configuration { get; private set; }
+
+        public Greeter(IConfiguration configuration)
+            {
+            _configuration = configuration;
+            }
+
+        public string GetGreeting()
+            {
+            return _configuration["Greeting"];
+            }
+        }
+    }
---
+++ Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace ModelRelief
+    {
+    public class Startup
+        {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+            {
+            var builder = new ConfigurationBuilder()
+                          .SetBasePath(env.ContentRootPath)
+                          .AddJsonFile("appsettings.json")
+                          .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+            {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
+            }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
+            {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+                {
+                app.UseDeveloperExceptionPage();
+                }
+
+            app.Run(async (context) =>
+                {
+                var message = greeter.GetGreeting();
+                await context.Response.WriteAsync(message);
+                });
+            }
+        }
+    }
