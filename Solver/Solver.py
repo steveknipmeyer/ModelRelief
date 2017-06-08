75c5029670ba74bc389d30699e119bfb5ab7f6be
dimensionsjewelry <steve@dimensionsjewelry.com>
2017-06-08 15:21:50 -0400

    Add parentt Visual Studio folder.

---
+++ Solver/Solver.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+im = plt.imread("Lucy.png")
+
+print (im.shape)
+
+def plti(im, h=8, **kwargs):
+    """
+    Helper function to plot an image.
+    Dimensions are in centimeters.
+    """
+    y = im.shape[0]
+    x = im.shape[1]
+    aspectRatio = y/x
+    w = aspectRatio * h
+    print ('Aspect ratio = {}'.format(aspectRatio))
+    print ("w = {}, h = {}".format (w, h))
+    
+    plt.figure(figsize=(w, h))
+    plt.imshow(im, interpolation="none", **kwargs)
+    plt.axis('off')
+
+#plti(im, 5)
+
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
