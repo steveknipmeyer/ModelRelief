baa1ec6a53681ec49f53e9864decdee6a791dbcf
steveknipmeyer <steve@knipmeyer.org>
2017-07-08 14:12:23 -0400

    Experiments with async and ViewComponents.

--- ModelRelief/ViewComponents/FooterViewComponent.cs
+++ ModelRelief/ViewComponents/FooterViewComponent.cs
@@ -16,11 +16,39 @@ namespace ModelRelief.ViewComponents
             {
             _greeter = greeter;
             }
-        
-        public async Task<IViewComponentResult> InvokeAsync()
+
+#if false        
+        public IViewComponentResult Invoke()
             {
+            // Invoke was removed from ViewComponent however this still compiles.
+
             var greetingMessage = _greeter.GetGreeting();
             return View ("Default", greetingMessage);
             }
+#endif
+
+#if false
+        public Task<IViewComponentResult> InvokeAsync()
+            {
+            // This form explicitly converts the return result to a Task.
+
+            var greetingMessage = _greeter.GetGreeting();
+            return Task.FromResult<IViewComponentResult>(View ("Default", greetingMessage));
+            }
+#endif
+#if true
+        public async Task<IViewComponentResult> InvokeAsync()
+            {
+            // This is the preferred form. It allows the use of await.
+            // The async modifier converts the return result to a Task.
+        
+            // Server Side Async is not Client Side Async
+            // https://stackoverflow.com/questions/36024748/viewcomponents-in-asp-net-mvc-6-are-not-async    
+            await Task.Delay(0);
+
+            var greetingMessage = _greeter.GetGreeting();
+            return View ("Default", greetingMessage);
+            }
+#endif
         }
     }
