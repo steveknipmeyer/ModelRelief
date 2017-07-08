399448284266331c3806f5296b02bbe685ece992
steveknipmeyer <steve@knipmeyer.org>
2017-07-08 13:52:15 -0400

    Add Footer ViewComponent

---
+++ ModelRelief/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ModelRelief.Services;
+
+namespace ModelRelief.ViewComponents
+    {
+    public class FooterViewComponent : ViewComponent
+        {
+        private IGreeter _greeter;
+
+        public FooterViewComponent(IGreeter greeter)
+            {
+            _greeter = greeter;
+            }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+            {
+            var greetingMessage = _greeter.GetGreeting();
+            return View ("Default", greetingMessage);
+            }
+        }
+    }
