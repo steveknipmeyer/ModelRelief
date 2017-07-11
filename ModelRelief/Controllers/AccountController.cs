f9ec81d8d9764efa8804b818035d976422daa99f
steveknipmeyer <steve@knipmeyer.org>
2017-07-11 07:49:47 -0400

    Add Register form.

---
+++ ModelRelief/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelRelief.ViewModels;
+
+namespace ModelRelief.Controllers
+        {
+    public class AccountController : Controller
+        {
+        [HttpGet]
+        public IActionResult Register()
+            {
+            return View();
+            }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel model)
+            {
+            if (!ModelState.IsValid)
+                {
+                // re-display with validation messages
+                return View();
+                }
+            return View();
+            }
+        }
+    }
