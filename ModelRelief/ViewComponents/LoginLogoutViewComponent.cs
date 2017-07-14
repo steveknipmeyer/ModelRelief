b168aae9733a4fa0b3d9994203337de9bda4e11f
steveknipmeyer <steve@knipmeyer.org>
2017-07-14 11:05:44 -0400

    Add LoginLogout ViewComponent.

---
+++ ModelRelief/ViewComponents/LoginLogoutViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ModelRelief.Services;
+
+namespace ModelRelief.ViewComponents
+    {
+    public class LoginLogoutViewComponent : ViewComponent
+        {
+        public async Task<IViewComponentResult> InvokeAsync()
+            {
+            return View ("Default");
+            }
+        }
+    }
