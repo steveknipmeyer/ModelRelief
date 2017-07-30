8017ee5961ac71f4287f8ff56c7f48f7b8725e52
steveknipmeyer <steve@knipmeyer.org>
2017-07-30 09:34:23 -0400

    Add DepthBuffer example.

--- ModelRelief/wwwroot/js/modelrelief.js
+++ ModelRelief/wwwroot/js/modelrelief.js
@@ -1264,6 +1264,104 @@ define("main", ["require", "exports", "three", "Viewer/Viewer", "ModelLoaders/OB
     var modelRelief = new ModelRelief();
     modelRelief.run();
 });
+define("Workbench/DepthTexture", ["require", "exports", "three", "Viewer/TrackballControls"], function (require, exports, THREE, TrackballControls_2) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var camera, scene, renderer, controls;
+    var target;
+    var postScene, postCamera;
+    var supportsExtension = true;
+    init();
+    animate();
+    function init() {
+        renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas') });
+        if (!renderer.extensions.get('WEBGL_depth_texture')) {
+            supportsExtension = false;
+            var element = document.querySelector('#error');
+            element.style.display = 'block';
+            return;
+        }
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 50);
+        camera.position.z = -4;
+        controls = new TrackballControls_2.TrackballControls(camera, renderer.domElement);
+        controls.enableDamping = true;
+        controls.dampingFactor = 0.25;
+        controls.rotateSpeed = 0.35;
+        // Create a multi render target with Float buffers
+        target = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+        target.texture.format = THREE.RGBFormat;
+        target.texture.minFilter = THREE.NearestFilter;
+        target.texture.magFilter = THREE.NearestFilter;
+        target.texture.generateMipmaps = false;
+        target.stencilBuffer = false;
+        target.depthBuffer = true;
+        target.depthTexture = new THREE.DepthTexture(window.innerWidth, window.innerHeight);
+        target.depthTexture.type = THREE.UnsignedShortType;
+        // Our scene
+        scene = new THREE.Scene();
+        setupScene();
+        // Setup post-processing step
+        setupPost();
+        onWindowResize();
+        window.addEventListener('resize', onWindowResize, false);
+    }
+    function setupPost() {
+        // Setup post processing stage
+        postCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+        var postMaterial = new THREE.ShaderMaterial({
+            vertexShader: document.querySelector('#post-vert').textContent.trim(),
+            fragmentShader: document.querySelector('#post-frag').textContent.trim(),
+            uniforms: {
+                cameraNear: { value: camera.near },
+                cameraFar: { value: camera.far },
+                tDiffuse: { value: target.texture },
+                tDepth: { value: target.depthTexture }
+            }
+        });
+        var postPlane = new THREE.PlaneGeometry(2, 2);
+        var postQuad = new THREE.Mesh(postPlane, postMaterial);
+        postScene = new THREE.Scene();
+        postScene.add(postQuad);
+    }
+    function setupScene() {
+        var diffuse = new THREE.TextureLoader().load('textures/brick_diffuse.jpg');
+        diffuse.wrapS = diffuse.wrapT = THREE.RepeatWrapping;
+        // Setup some geometries
+        var geometry = new THREE.TorusKnotGeometry(1, 0.3, 128, 64);
+        var material = new THREE.MeshBasicMaterial({ color: 'blue' });
+        var count = 50;
+        var scale = 5;
+        for (var i = 0; i < count; i++) {
+            var r = Math.random() * 2.0 * Math.PI;
+            var z = (Math.random() * 2.0) - 1.0;
+            var zScale = Math.sqrt(1.0 - z * z) * scale;
+            var mesh = new THREE.Mesh(geometry, material);
+            mesh.position.set(Math.cos(r) * zScale, Math.sin(r) * zScale, z * scale);
+            mesh.rotation.set(Math.random(), Math.random(), Math.random());
+            scene.add(mesh);
+        }
+    }
+    function onWindowResize() {
+        var aspect = window.innerWidth / window.innerHeight;
+        camera.aspect = aspect;
+        camera.updateProjectionMatrix();
+        var dpr = renderer.getPixelRatio();
+        target.setSize(window.innerWidth * dpr, window.innerHeight * dpr);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    function animate() {
+        if (!supportsExtension)
+            return;
+        requestAnimationFrame(animate);
+        controls.update();
+        // render scene into target
+        renderer.render(scene, camera, target);
+        // render post FX
+        renderer.render(postScene, postCamera);
+    }
+});
 define("Viewer/Graphics", ["require", "exports", "three"], function (require, exports, THREE) {
     // ------------------------------------------------------------------------// 
     // ModelRelief                                                             //
