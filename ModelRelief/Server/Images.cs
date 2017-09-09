f218169ebb89a4dc369b27be7fc7d8ab0bad130e
steveknipmeyer <steve@knipmeyer.org>
2017-09-09 19:06:49 -0400

    Add ImageSharp reference.

---
+++ ModelRelief/Server/Images.cs
@@ -0,0 +1,21 @@
+// ------------------------------------------------------------------------// 
+// ModelRelief                                                             //
+//                                                                         //                                                                          
+// Copyright (c) <2017> Steve Knipmeyer                                    //
+// ------------------------------------------------------------------------//
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using ImageSharp;
+
+namespace ModelRelief.Server
+{
+    public class Images
+    {
+
+    }
+}    
+    
