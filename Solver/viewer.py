712a0dcdb67966d1a72752e1e485196ce4c9f645
steveknipmeyer <steve@knipmeyer.org>
2018-04-23 05:47:27 -0400

    Remove unused fileName parameter from Dispatcher methods. Remove unused working paramter from viewer.py.

--- Solver/viewer.py
+++ Solver/viewer.py
@@ -27,17 +27,12 @@ class Viewer:
     """
     Displays a depth buffer as an image.
     """
-    def __init__(self, settings, working):
+    def __init__(self, settings):
         """
             Iniitalize an instance of the Viewer.
         """
         with open(settings) as json_file:
             self.settings = json.load(json_file)
-        
-        working_folder = os.path.abspath(working)
-        self.working_folder = working_folder
-        if not os.path.exists(working_folder):
-            os.makedirs(working_folder)
 
         self.depth_buffer = DepthBuffer(self.settings['DepthBuffer'])
 
@@ -46,6 +41,7 @@ class Viewer:
         Display a depth buffer as an iamge.
         """
         # read raw bytes
+        print("DB path = %s" % self.depth_buffer.path)
         byte_depths = self.depth_buffer.read__binary(self.depth_buffer.path)
         # convert to floats
         floats = self.depth_buffer.unpack_floats(byte_depths)
@@ -80,11 +76,9 @@ def main():
     options_parser = argparse.ArgumentParser()
     options_parser.add_argument('--settings', '-s',
                                 help='Mesh JSON settings file that defines the associated DepthBuffer and MeshTransform.', required=True)
-    options_parser.add_argument('--working', '-w',
-                                help='Temporary working folder.', required=True)
     arguments = options_parser.parse_args()
 
-    viewer = Viewer(arguments.settings, arguments.working)
+    viewer = Viewer(arguments.settings)
     viewer.show_image(viewer.depth_buffer)
 
     noop = 1
