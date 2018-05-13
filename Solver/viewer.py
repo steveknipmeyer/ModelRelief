f85a4d373f2648004139cd4f15c2257b0f0e8a6e
steveknipmeyer <steve@knipmeyer.org>
2018-05-13 18:58:59 -0400

    Group workbench images in ScrollableWindow.

--- Solver/viewer.py
+++ Solver/viewer.py
@@ -10,8 +10,13 @@
 
 .. moduleauthor:: Steve Knipmeyer <steve@knipmeyer.org>
 """
-
+import matplotlib
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
+from PyQt5 import QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
 import numpy as np
 
 class Viewer:
@@ -65,7 +70,7 @@ class Viewer:
         assert((titles is None) or (len(images) == len(titles)))
         n_images = len(images)
         if titles is None: titles = ['Image (%d)' % i for i in range(1, n_images + 1)]
-        fig = plt.figure(figsize=(15, 4))
+        fig = plt.figure()
 
         columns = np.ceil(n_images/float(rows))
         for n, (image, title, cmap) in enumerate(zip(images, titles, cmaps)):
@@ -77,5 +82,33 @@ class Viewer:
             plt.imshow(image, cmap)
             sub_plot.set_title(title)
 
-        #fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
-        plt.show()
+        image_dimension = 10
+        fig.set_size_inches(n_images * image_dimension , image_dimension)       
+        window = ScrollableWindow(fig)        
+        #plt.show()
+
+class ScrollableWindow(QtWidgets.QMainWindow):
+    """ https://stackoverflow.com/questions/42622146/scrollbar-on-matplotlib-showing-page """
+
+    def __init__(self, fig):
+        self.qapp = QtWidgets.QApplication([])
+
+        QtWidgets.QMainWindow.__init__(self)
+        self.widget = QtWidgets.QWidget()
+        self.setCentralWidget(self.widget)
+        self.widget.setLayout(QtWidgets.QVBoxLayout())
+        self.widget.layout().setContentsMargins(0,0,0,0)
+        self.widget.layout().setSpacing(0)
+
+        self.fig = fig
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.draw()
+        self.scroll = QtWidgets.QScrollArea(self.widget)
+        self.scroll.setWidget(self.canvas)
+
+        self.nav = NavigationToolbar(self.canvas, self.widget)
+        self.widget.layout().addWidget(self.nav)
+        self.widget.layout().addWidget(self.scroll)
+
+        self.show()
+        exit(self.qapp.exec_()) 
