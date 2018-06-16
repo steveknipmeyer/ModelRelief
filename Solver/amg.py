8357598208de181909a1fa1827496d0ab31d7994
steveknipmeyer <steve@knipmeyer.org>
2018-06-16 09:41:50 -0400

    Add AMG example.

---
+++ Solver/amg.py
@@ -0,0 +1,23 @@
+import pyamg
+import numpy as np
+
+def tutorial():
+    A = pyamg.gallery.poisson((500,500), format='csr')  # 2D Poisson problem on 500x500 grid
+    ml = pyamg.ruge_stuben_solver(A)                    # construct the multigrid hierarchy
+    print(ml)                                           # print hierarchy information
+    b = np.random.rand(A.shape[0])                      # pick a random right hand side
+    x = ml.solve(b, tol=1e-10)                          # solve Ax=b to a tolerance of 1e-10
+    print("residual: ", np.linalg.norm(b-A*x))          # compute norm of residual vector
+
+def experiment():
+    A = pyamg.gallery.poisson((4,4), format='csr')      # 2D Poisson problem on 4x4 grid
+    ml = pyamg.ruge_stuben_solver(A)                    # construct the multigrid hierarchy
+    print(ml)                                           # print hierarchy information
+
+    b = np.random.rand(A.shape[0])                      # pick a random right hand side
+    x = ml.solve(b, tol=1e-10)                          # solve Ax=b to a tolerance of 1e-10
+
+    print("residual: ", np.linalg.norm(b-A*x))          # compute norm of residual vector
+    pass
+
+experiment()
