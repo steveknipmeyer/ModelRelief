456f76f7ff53ae40670e4eedb1a73bac8b7df5a7
steveknipmeyer <steve@knipmeyer.org>
2018-09-05 10:47:56 -0400

    Extend ReliefTest to decorate test methods with names and intermediate results.

---
+++ Relief/tests/python/relieftest.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python 
+#
+#   Copyright (c) 2018
+#   All Rights Reserved.
+#
+"""
+.. module:: relief_test
+   :synopsis: Unit tests for relief Pyhon C++ extensions.
+
+.. moduleauthor:: Steve Knipmeyer <steve@knipmeyer.org>
+"""
+import colorama
+import math
+import numpy as np
+import os
+import subprocess
+import unittest
+from functools import wraps
+
+import relief 
+from logger import Logger
+from stopwatch import benchmark
+from tools import Colors, Tools
+
+def display_tag(tag_name: str = None, color: str = Colors.BrightCyan):
+    """ A decorator for displaying the active test name.
+    N.B. These arguments are available to the wrapped method through closure.
+    Parameters
+    ----------
+    tag_name
+        Tag to display.
+    color   
+        Color for the logger message.        
+    """            
+    def decorator_maker(fn):
+        @wraps(fn)
+        def wrapped(*args, **kwargs):
+            tag = fn.__name__ if tag_name is None else tag_name
+            # *args is the argument list to the wrapped function. For a class method, the first argument is 'self'.
+            function_self = args[0]
+            function_self.logger.logInformation(tag, color)
+            result = fn(*args, **kwargs)
+            return result
+        return wrapped
+    return decorator_maker
+
+class ReliefTest(unittest.TestCase):
+
+    def __init__(self, methodName='runTest'):
+        """ Initialize an instance of ReliefTest. """
+        super().__init__(methodName)
+
+        colorama.init()
+        self.logger = Logger()
+
+    def tearDown(self):
+        """ 
+        This method is called by the test runner after the execution of each test.
+        The results are inspected and a log message is created if the test was not successful.
+        # https://stackoverflow.com/questions/4414234/getting-pythons-unittest-results-in-a-teardown-method
+        """
+
+        # populate result
+        result = self.defaultTestResult() 
+        self._feedErrorsToResult(result, self._outcome.errors)
+
+        # errors   = runtime errors (invalid method execution)
+        # failures = unsuccessful test results
+        error = self.get_exception_list(result.errors)
+        failure = self.get_exception_list(result.failures)
+        ok = not error and not failure
+
+        if not ok:
+            result_type, result_text = ('ERROR', error) if error else ('FAIL', failure)
+            # take last line that does not begin with a space, skipping first line
+            result_reason = [x for x in result_text.split('\n')[1:] if not x.startswith(' ')][0]
+            message = f"{result_type}: {self.id()}\n     {result_reason}"
+            self.logger.logError(message)
+
+    def get_exception_list(self, cumulative_exception_list):
+        """
+        Returns the exception list associated with an unsuccessful result of a test method.
+        unittest accumulates the test case results into a list. So, for the current test we check if the last method matches ourselves.
+        Parameters
+        ---------
+        cumulative_exception_list
+            <Cumulative> list of tuples (method, exception list) for all the test methods that have been executed.
+        """
+        # last entry is ourselves?
+        if cumulative_exception_list and cumulative_exception_list[-1][0] is self:
+            # return the exception list
+            return cumulative_exception_list[-1][1]
+
+    @display_tag('\nMiscellaneous', Colors.BrightMagenta)
+    @display_tag()
+    def test_FillNumpyArray(self):
+        """ Fills a Numpy array with a constant value."""
+        a = np.zeros((512, 512))
+        value = 1.0
+        filled = relief.fill(a, value)
+        self.assertEqual(filled[256, 256], 0)
+
+    @display_tag('\nGaussianKernel', Colors.BrightMagenta)
+    @display_tag()
+    def test_GaussianKernelOriginEquals1(self):
+        """ Verifies the origin (center) of the kernel is equal to 1.0."""
+        k = relief.GaussianKernel(1.0)
+        self.assertAlmostEqual(k.Element(0, 0), 0.16210282163712664, places = 6)
+        
+    @display_tag('\nC++', Colors.BrightMagenta)
+    @display_tag()
+    def test_RunCPlusPlusUnitTests(self):
+        """ Executes the C++ unit tests."""
+        root = os.path.dirname(os.path.realpath(__file__))
+        executable = os.path.join(root, '..', 'bin', 'reliefUnitTests.exe')
+        status = Tools.exec(executable)
+        self.assertEqual(status, 1)
+
+if __name__ == '__main__':
+    unittest.main()
