89381e15901fdb3b8d67f67a05074f83ebaa3ae8
Steve Knipmeyer <steve@knipmeyer.org>
2019-04-26 15:26:48 -0400

    Resolve 'conda activate env' issue in Builder

--- Build/BuildReliefPythonExtensions.sh
+++ Build/BuildReliefPythonExtensions.sh
@@ -2,9 +2,9 @@
 # Builds the Python C++ extensions.
 # usage: BuildReliefPythonExtensions <Development | Production>
 echo BuildReliefPythonExtensions
-echo 0=$0
-echo 1=$1
-echo 2=$2
+echo '$0'=$0
+echo '$1'=$1
+echo '$2'=$2
 
 if [ $# -eq 0 ]; then
     echo "usage: $0 [Development | Production]"
@@ -29,12 +29,19 @@ cd $MRSolution
 # set up Production Python environement
 if [ $EnvironmentName = "mrenv" ]; then
     echo "Activating production (mrenv) Python environment"
+    # https://github.com/conda/conda/issues/7980
+    source ~/anaconda3/etc/profile.d/conda.sh
     conda activate Publish/mrenv
  fi
+
 cd Relief
-#python setup.py build $BuildOptions install
+python setup.py build $BuildOptions install
+
+# restore Development environment
 cd ..
 if [ $EnvironmentName = "mrenv" ]; then
     echo "Restoring developoment (devenv) Python environment"
+    # https://github.com/conda/conda/issues/7980
+    source ~/anaconda3/etc/profile.d/conda.sh
     conda activate ./devenv
  fi
--- Build/Builder.py
+++ Build/Builder.py
@@ -14,8 +14,11 @@
 """
 import argparse
 import ast
+import shlex
 import os
+import pprint
 import shutil
+import subprocess
 import sys
 from enum import Enum
 
@@ -178,7 +181,7 @@ class Builder:
         self.logger.logInformation("\nPython C++ extensions", Colors.BrightMagenta)
         environment = RuntimeEnvironment.production.value if self.publish else RuntimeEnvironment.development.value
         os.chdir(self.solution_path)
-        Tools.exec(f". BuildReliefPythonExtensions.sh {environment}")
+        Tools.exec(f"BuildReliefPythonExtensions.sh {environment}")
 
         # database initialization and user store
         if self.arguments.initialize:
@@ -308,9 +311,6 @@ class Builder:
         self.environment.push()
 
         self.initialize(self.source_wwwroot_path, self.source_store_path, self.publish_path)
-
-        Tools.exec(f"BuildReliefPythonExtensions.sh Production")
-        return
 
         # build
         self.build()
